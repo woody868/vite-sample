371d52688af386ea346cb9a9545af165cef88a79
woody868 <lhj303606@gmail.com>
2025-08-12 11:16:36 +0900

    ?

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) //구조분해할당?//
 
   return (
     <>
